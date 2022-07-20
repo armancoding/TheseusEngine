841dfcbf521d5e54371ec27b6ba7530603a5dbfe
armancoding <arman.armament@gmail.com>
2022-07-20 01:30:02 -0400

    First Commit

---
+++ TheseusEditor/Project/NewProjectClass.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace TheseusEditor.Project
+{
+    [DataContract]
+    public class ProjectTemplate
+    {
+        [DataMember]
+        public string ProjectType { get; set; }
+        [DataMember]
+        public string ProjectFile { get; set; }
+        [DataMember]
+        public List<string> Folders { get; set; }
+    }
+
+    class NewProjectClass : ViewModelBase
+    {
+        //TODO get the path from the instalation location
+        private readonly string _templatePath = @"..\..\TheseusEditor\ProjectTemplates";
+        private string _name = "New Project";
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+        private string _path = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\Theseus Projects\";
+        public string Path
+        {
+            get => _path;
+            set
+            {
+                if (_path != value)
+                {
+                    _path = value;
+                    OnPropertyChanged(nameof(Path));
+                }
+            }
+        }
+
+        public NewProjectClass()
+        {
+            try
+            {
+                var templatesFiles = Directory.GetFiles(_templatePath, "template.xml", SearchOption.AllDirectories);
+                Debug.Assert(templatesFiles.Any());
+                foreach (var file in templatesFiles)
+                {
+                    var template = new ProjectTemplate()
+                    {
+                        ProjectType = "Empty Project",
+                        ProjectFile = "project.theseus",
+                        Folders = new List<string>() { ".Theseus", "Content", "GameCode" }
+                    };
+
+                }
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                //TODO: log error
+            }
+        }
+    }
+}
